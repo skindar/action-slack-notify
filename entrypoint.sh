261aca044d6bce2d1464b5ae35fda7c820e02a07
Valentyn Fiedietsov <fedetsov@gmail.com>
2020-03-11 10:42:50 +0200

    testing configuration varriants

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 
+#initialise secret
+
 # Check required env variables
+service_account_key=$1
 flag=0
-if [[ -z "$SLACK_WEBHOOK" ]]; then
+if [[ -z "$service_account_key" ]]; then
     flag=1
-    missing_secret="SLACK_WEBHOOK"
-    if [[ -n "$VAULT_ADDR" ]] && [[ -n "$VAULT_TOKEN" ]]; then
-        flag=0
-    fi
-    if [[ -n "$VAULT_ADDR" ]] || [[ -n "$VAULT_TOKEN" ]]; then
-        missing_secret="VAULT_ADDR and/or VAULT_TOKEN"
-    fi
 fi
 
 if [[ "$flag" -eq 1 ]]; then
